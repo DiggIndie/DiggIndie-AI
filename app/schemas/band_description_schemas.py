491302c6bd9fb143b0fbdb8444afb2ba56f3d940
Hoyoung027 <hybyun0207@gmail.com>
2025-12-16 17:35:38 +0900

    fix : dto 변수명 수정

--- app/schemas/band_description_schemas.py
+++ app/schemas/band_description_schemas.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 class BandDescriptionResponse(BaseModel):
     band_id: int
     description: Optional[str] = None
-    created_at: Optional[datetime] = None
-    updated_at: Optional[datetime] = None
-    deleted_at: Optional[datetime] = None
+    createdAt: Optional[datetime] = None
+    updatedAt: Optional[datetime] = None
+    deletedAt: Optional[datetime] = None
     embedding: Optional[List[float]] = None
